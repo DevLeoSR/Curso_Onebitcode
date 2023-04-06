53873003413aef734f9a27748c65e319a1d4ed52
Leonardo de Souza Rezende <devleozaosr@gmail.com>
2023-04-06 18:13:01 -0300

    conclusão do exercicio

--- 3º exercicios/script.js
+++ 3º exercicios/script.js
@@ -1,4 +1,12 @@
-const  veiculo01 = prompt('qual o nome do primeiro veiculo?')
-const  velocidade01 = prompt('qual a velocidade do primeiro veiculo?')
-const  veiculo02 = prompt('qual o nome do segundo veiculo?')
-const  velocidade02 = prompt('qual a velociade do segundo veiculo?')
+const veiculo01 = prompt("qual o nome do primeiro veiculo?");
+const velocidade01 = prompt("qual a velocidade do primeiro veiculo?");
+const veiculo02 = prompt("qual o nome do segundo veiculo?");
+const velocidade02 = prompt("qual a velociade do segundo veiculo?");
+
+if (velocidade01 > velocidade02) {
+   alert("o veiculo " + veiculo01 + " é mais rapido");
+} else if (velocidade01 < velocidade02) {
+   alert("o veiculo " + veiculo02 + " é mais rapido");
+} else {
+   alert("os veiculos estão na mesma velocidade");
+}
