c5b43519ba4d9f00464a82109e5c979647287005
Leonardo de Souza Rezende <devleozaosr@gmail.com>
2023-04-06 16:40:21 -0300

    Add exercicio 3

--- 3º exercicios/script.js
+++ 3º exercicios/script.js
@@ -1,4 +1,4 @@
-const = veiculo01 = prompt('qual o nome do primeiro veiculo?')
-const = velocidade01 = prompt('qual a velocidade do primeiro veiculo?')
-const = veiculo02 = prompt('qual o nome do segundo veiculo?')
-const = velocidade02 = prompt('qual a velociade do segundo veiculo?')
+const  veiculo01 = prompt('qual o nome do primeiro veiculo?')
+const  velocidade01 = prompt('qual a velocidade do primeiro veiculo?')
+const  veiculo02 = prompt('qual o nome do segundo veiculo?')
+const  velocidade02 = prompt('qual a velociade do segundo veiculo?')
