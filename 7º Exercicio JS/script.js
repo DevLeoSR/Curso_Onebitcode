97de427502f24a3328c15c83251b5541b6db7eaf
Leonardo de Souza Rezende <devleozaosr@gmail.com>
2023-04-06 22:37:58 -0300

    exercicio 6 e 7

--- 7º Exercicio JS/script.js
+++ 7º Exercicio JS/script.js
@@ -1,19 +1,15 @@
-let dinheiroTotal =parseFloat(prompt('quantidade de dinheiro inicial:'));
+let dinheiroTotal = parseFloat(prompt('quantidade de dinheiro inicial:'));
 let opcao = '';
-let valor = 0;
-
 
 do {
    opcao = prompt('O valor total é: R$'+ dinheiroTotal +'\nopções:\n' + '1 - Adicionar dinheiro\n' + '2 - Retirar dinheiro\n' + '3 - Sair')
 
    switch (opcao) {
       case '1':
-         valor=(prompt('qual valor a ser Depositado:'))
-         dinheiroTotal += parseFloat(valor) 
+         dinheiroTotal += parseFloat(prompt('qual valor a ser Depositado:')) 
          break;
       case '2':
-         valor=(prompt('qual valor a ser Depositado:'))
-         dinheiroTotal -= parseFloat(valor)
+         dinheiroTotal -= parseFloat(prompt('qual valor a ser Depositado:'))
          break;
       case '3':
          alert('Saindo...')
