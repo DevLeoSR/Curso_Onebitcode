22b18561d91273ebc1cd3e73518d6bbfaffb92a5
Leonardo de Souza Rezende <devleozaosr@gmail.com>
2023-04-07 16:06:39 -0300

    exercicio 11

---
+++ 11º Exercicio JS/script.js
@@ -0,0 +1,34 @@
+const baralho = [];
+let Pilha = '';
+let opcao;
+
+do {
+   opcao = prompt('No baralho consta as cartas: \n' + Pilha + '\n' +
+   '\n1 - Adicionar Carta' +
+   '\n2 - Puxar uma carta' +
+   '\n3 - Sair');   // Menu do app
+
+   Pilha = ''; // reset do acumulador
+
+   switch (opcao) {
+      case '1':
+         baralho.unshift(prompt('qual carta deseja adicionar:'));
+         //baralho.push(prompt('qual carta deseja adicionar:'));  // outra forma de ser feito
+         break;
+      case '2':
+         alert('a carta puxada foi: ' + baralho.shift());
+         //alert('a carta puxada foi: ' + baralho.pop());  // outra forma de ser feito
+         break;
+      case '3':
+         alert('Saindo do jogo...')  
+         break;
+      default: alert('Opção  inválida...')    
+   }
+   // acumulador
+   for (let index = 0; index < baralho.length; index++) {
+         Pilha += '\n' + baralho[index];
+         //Pilha +=  baralho[index] + '\n';  // outra forma de ser feito
+   }; 
+
+} while (opcao != 3); 
+
