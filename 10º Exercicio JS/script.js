cbb7fd4d0a3ecad3a5922dacb97d0724f6af589a
Leonardo de Souza Rezende <devleozaosr@gmail.com>
2023-04-07 15:33:50 -0300

    pequena refatoração

--- 10º Exercicio JS/script.js
+++ 10º Exercicio JS/script.js
@@ -1,29 +1,23 @@
 const pacientes = [];
 let opcao 
-let nome
 let Fila = '';
 
 do { 
    opcao = prompt(
-   'Bem vindo ao consultório!\n'+
-   'Fila de Espera:\n\n' + Fila +'\n\n' +
+   'Bem vindo ao consultório!\n\n'+
+   'Fila de Espera:\n' + Fila +'\n' +
    '1 - Novo paciente\n' +
    '2 - Chamar o paciente\n' +
    '3 - Sair');
-   
+
    Fila = "";
 
    switch (opcao) {      
       case'1':
-         nome = prompt('qual o nome do paciente?');
-         pacientes.push(nome);
-         alert('O paciente ' + nome + ' foi  adicionado');
-         nome = '';
+         pacientes.push(prompt('qual o nome do paciente?'));
          break;   
       case '2':
-         nome = pacientes.shift();
-         alert('Chamando o paciente: ' + nome);
-         nome = '';
+         alert('Chamando o paciente: ' + pacientes.shift());         
          break;
       case '3':
          alert('Saindo...');
@@ -32,8 +26,8 @@ do {
          alert('opção inválida');
    }
 
-   for (let index = 0; index < pacientes.length; index++) {
-      Fila += (index + 1) + 'º - ' + pacientes[index] + '\n';
+   for (let i = 0; i < pacientes.length; i++) {
+      Fila += (i + 1) + 'º - ' + pacientes[i] + '\n';
    }
 
 } while (opcao != 3);
